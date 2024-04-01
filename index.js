8094fc78109940671966ef2f26aeaf98cbfbef2c
Kunark Khewal <kunark.khewal88@gmail.com>
2024-04-01 14:06:14 +0530

    added schdeuled message feature, and tried to deleted scheduled message but didnt worked

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const amqplib = require("amqplib");
+const uuid = require('uuid');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const RMQ_URL = 'amqp://localhost'
 
 app.get('/send', async (req, res) => {
     console.log('HIT SEND ROUTE')
@@ -17,6 +20,15 @@ app.get('/get', async (req, res) => {
     res.send(200)
 })
 
+app.delete('/delete-delayed-message/:id', async (req, res) => {
+    const msgId = req.params.id;
+    console.log('HIT DELETE ROUTE')
+    await cancelScheduledMessages(msgId);
+    res.send(200)
+})
+
+
+
 
 
 app.listen(PORT, () => {
@@ -25,7 +37,7 @@ app.listen(PORT, () => {
 
 
 async function sendMessageToRabbitMQ() {
-    const queueName = 'queue-test';
+    const queueName = 'scheduler-queue-test';
     const message = 'testing ' + new Date();
     console.log(message);
     const opt = { credentials: amqplib.credentials.plain(USERNAME, PASSWORD) };
@@ -36,24 +48,50 @@ async function sendMessageToRabbitMQ() {
 
     // // direct exchange
     // const exchangeName = 'test_direct_exchange';
-    // await channel.assertExchange(exchangeName, 'direct', { durable: true });
+    await channel.assertExchange('delay-exchange', 'x-delayed-message', { durable: true, arguments: {"x-delayed-type": "direct"} });
+    // const args = {
+    //     "x-delayed-type": "direct"
+    // }
+    // await channel.exchangeDeclare("delay-exchange", "x-delayed-message", true, false, args);
     
     // // bind the queue to the exchange
-    // const routingKey = 'test_routing_key';
-    // await channel.bindQueue(queueName, exchangeName, routingKey);
+    const routingKey = 'delay_routing_key';
+    await channel.bindQueue(queueName, 'delay-exchange', routingKey);
 
 
-    const data = await channel.sendToQueue(
-        queueName,
+    // const data = await channel.sendToQueue(
+    //     queueName,
+    //     Buffer.from(message),
+    //     { 
+    //         persistent: true,
+    //         expiration: 10000
+    //         // headers: {
+    //         //     "x-delay": 5000
+    //         // } 
+    //     }
+    // );
+
+    const messageId = uuid.v4(); // Generate a unique message ID
+    const data = await channel.publish(
+        'delay-exchange',
+        routingKey,
         Buffer.from(message),
-        { persistent: true }
-    );
+        { 
+            persistent: true,
+            headers: {
+                "x-delay": 20000,
+                'x-message-id': messageId
+            } 
+        }
+    )
 
     console.log('data => ',data);
+    console.log('messageId => ',messageId);
+
 }
 
 async function getMessageFromRabbitMQ() {
-    const queueName = 'queue-test';
+    const queueName = 'scheduler-queue-test';
     const opt = { credentials: amqplib.credentials.plain(USERNAME, PASSWORD) };
     const connection = await amqplib.connect(RMQ_URL, opt);
     
@@ -68,4 +106,20 @@ async function getMessageFromRabbitMQ() {
             console.log('Consumer cancelled by server');
         }
     })
+}
+
+// Cancel scheduled messages (set TTL to 0)
+async function cancelScheduledMessages(messageId) {
+    try {
+        // const queueName = 'scheduler-queue-test';
+        const connection = await amqplib.connect(RMQ_URL);
+        const channel = await connection.createChannel();
+        // Re-publish the message with a TTL of 0 to cancel it
+        await channel.publish('delay-exchange', 'delay_routing_key', Buffer.from(''), {
+            headers: { 'x-delay': 0, 'x-message-id': messageId }
+        });
+        console.log(`Cancelled scheduled message with ID: ${messageId}`);
+    } catch (error) {
+        console.error('Error cancelling scheduled message:', error);
+    }
 }
